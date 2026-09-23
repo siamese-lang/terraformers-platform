edc9eb87e9bb1105c4f5d94f117356768190db92
siamese-lang <askj95@naver.com>
2026-09-23 10:39:21 +0900

    fix: make identity migration portable

--- backend/src/main/resources/db/migration/V20260923_005__neutralize_external_user_identity.sql
+++ backend/src/main/resources/db/migration/V20260923_005__neutralize_external_user_identity.sql
@@ -1,5 +1,7 @@
 ALTER TABLE users
-    ADD COLUMN external_identity_provider VARCHAR(64) NULL,
+    ADD COLUMN external_identity_provider VARCHAR(64) NULL;
+
+ALTER TABLE users
     ADD COLUMN external_identity_subject VARCHAR(128) NULL;
 
 UPDATE users
@@ -10,8 +12,14 @@ WHERE external_identity_provider IS NULL
   AND cognito_sub IS NOT NULL;
 
 ALTER TABLE users
-    MODIFY COLUMN external_identity_provider VARCHAR(64) NOT NULL,
-    MODIFY COLUMN external_identity_subject VARCHAR(128) NOT NULL,
-    MODIFY COLUMN cognito_sub VARCHAR(128) NULL,
+    MODIFY COLUMN external_identity_provider VARCHAR(64) NOT NULL;
+
+ALTER TABLE users
+    MODIFY COLUMN external_identity_subject VARCHAR(128) NOT NULL;
+
+ALTER TABLE users
+    MODIFY COLUMN cognito_sub VARCHAR(128) NULL;
+
+ALTER TABLE users
     ADD CONSTRAINT uk_users_external_identity
         UNIQUE (external_identity_provider, external_identity_subject);

99c8244648aae5a42cb6ad1ae4c1e6cc3e207469
siamese-lang <askj95@naver.com>
2026-09-24 10:06:39 +0900

    refactor: add provider-neutral generation format signal

---
+++ backend/src/main/java/com/terraformers/modernization/analysis/AnalysisGenerationResponseFormatException.java
@@ -0,0 +1,12 @@
+package com.terraformers.modernization.analysis;
+
+public class AnalysisGenerationResponseFormatException extends RuntimeException {
+
+    public AnalysisGenerationResponseFormatException(String message) {
+        super(message);
+    }
+
+    public AnalysisGenerationResponseFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
